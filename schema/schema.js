2890147dabd9083dd72441d830582510b8229fa1
Gagandeep Singh <gagan.ddun@gmail.com>
2020-03-18 19:03:18 +1100

    added user schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -12,4 +19,23 @@ const UserType = new GraphQLObjectType({
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt }
     }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
