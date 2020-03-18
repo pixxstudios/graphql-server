9d0ecc83ddc8847164da89ce3bbee730b79093ea
Gagandeep Singh <gagan.ddun@gmail.com>
2020-03-18 19:29:21 +1100

    axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -25,7 +25,8 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id })
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                .then(result => result.data);
             }
         }
     }
